4ec9cfc3c1afc2a04b08e12ce752c026af9c889d
akinloluwami <akinkunmioye42@gmail.com>
2025-12-14 00:42:02 +0100

    feat: implement session handling in main route component

--- apps/web/src/routes/index.tsx
+++ apps/web/src/routes/index.tsx
@@ -1,5 +1,18 @@
 import { createFileRoute, Navigate } from "@tanstack/react-router";
+import { authClient } from "../lib/auth-client";
 
 export const Route = createFileRoute("/")({
-  component: () => <Navigate to="/dash" replace />,
+
+
+  component: () => {
+const {data, isPending} = authClient.useSession();
+
+if (isPending) return <div>Loading...</div>
+
+if (!data?.session) {
+  return <Navigate to="/login" replace />
+}
+
+    return  <Navigate to="/dash" replace />
+  },
 });
