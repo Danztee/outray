0f7dfbf91b8c6305247794b3c89193f953bf75c7
akinloluwami <akinkunmioye42@gmail.com>
2025-12-14 00:41:54 +0100

    feat: add organization plugin to betterAuth configuration

--- apps/web/src/lib/auth.ts
+++ apps/web/src/lib/auth.ts
@@ -1,6 +1,7 @@
 import { betterAuth } from "better-auth";
 import { drizzleAdapter } from "better-auth/adapters/drizzle";
 import { db } from "../db";
+import { organization } from "better-auth/plugins"
 
 export const auth = betterAuth({
   database: drizzleAdapter(db, {
@@ -12,5 +13,10 @@ export const auth = betterAuth({
       clientId: process.env.GITHUB_CLIENT_ID as string,
       clientSecret: process.env.GITHUB_CLIENT_SECRET as string,
     },
+    google: {
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+    },
   },
+  plugins:[organization()],
 });
