927d081b80b63a38a20a6509aa5390a7f345f567
akinloluwami <akinkunmioye42@gmail.com>
2025-12-27 21:46:15 +0100

    feat: implement Sidebar component in $orgSlug route

--- apps/web/src/routes/$orgSlug.tsx
+++ apps/web/src/routes/$orgSlug.tsx
@@ -1,7 +1,7 @@
 import { createFileRoute, Outlet, Navigate } from "@tanstack/react-router";
 import { useState } from "react";
 import { authClient } from "../lib/auth-client";
-import { Sidebar } from "../components/sidebar";
+import { Sidebar } from "../components/app-sidebar";
 
 export const Route = createFileRoute("/$orgSlug")({
   component: DashboardLayout,
